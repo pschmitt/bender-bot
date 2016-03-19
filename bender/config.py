d692a4189eeacc104f00d9f44ce527399920c265
Philipp Schmitt <philipp@schmitt.co>
2016-03-19 08:37:43 +0100

    Package

--- bender/config.py
+++ bender/config.py
@@ -3,17 +3,17 @@
 
 import yaml
 
-def get_config(config_file='bender.yaml'):
+def get_config(config_file='../bender.yaml'):
     with open(config_file) as f:
         config = yaml.load(f)
     return config
 
-def get_config_section(config_file='bender.yaml', section='bot'):
+def get_config_section(config_file='../bender.yaml', section='bot'):
     return get_config(config_file)[section]
 
-def get_token(config_file='bender.yaml'):
+def get_token(config_file='../bender.yaml'):
     return get_config_section(config_file, 'bot')['token']
 
-def get_authorized_users(config_file='bender.yaml'):
+def get_authorized_users(config_file='../bender.yaml'):
     return get_config_section(config_file, 'bot')['authorized_users']
 
